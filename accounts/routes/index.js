8822997f3215cdc3955c28dff175d56595d99970
LinC3e <ivancostilla17@gmail.com>
2024-03-13 14:05:38 -0300

    Saving information

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync(__dirname + '/../data/db.json');
+const db = low(adapter)
+
+const shortid = require('shortid');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -15,7 +23,11 @@ router.get('/account/create', function(req, res, next) {
 });
 
 router.post('/account', (req,res) => {
+
+  let id = shortid.generate();
+
   console.log(req.body)
+  db.get('accounts').push({ id: id, ...req.body }).write();
   res.send('Account send')
 })
 
