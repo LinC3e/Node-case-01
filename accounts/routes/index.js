b77912dba55b60bbf171904fe3803ea2c0070c16
LinC3e <ivancostilla17@gmail.com>
2024-03-18 17:10:52 -0300

    delete data

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -14,16 +14,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET acc-list page. */
 router.get('/account', function(req, res, next) {
   let accounts = db.get('accounts').value();
   console.log(accounts)
   res.render('list', {accounts : accounts } );
 });
 
+/* GET form-create page. */
 router.get('/account/create', function(req, res, next) {
   res.render('create');
 });
 
+/* POST create acc */
 router.post('/account', (req,res) => {
 
   let id = shortid.generate();
@@ -31,7 +34,19 @@ router.post('/account', (req,res) => {
   console.log(req.body)
   db.get('accounts').push({ id: id, ...req.body }).write();
   res.render('success', {
-    url: '/account'
+    url: '/account',
+    msg: 'Created'
+  })
+})
+
+router.get('/account/:id', (req, res) => {
+  let id = req.params.id;
+
+  db.get('accounts').remove({ id: id }).write();
+
+  res.render('success', {
+    url: '/account',
+    msg: 'Deleted'
   })
 })
 
